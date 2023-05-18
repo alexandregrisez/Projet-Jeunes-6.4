accc62368f1d615fecf18e2e73864ab3d9dc19e3
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2023-05-18 12:16:42 +0200

    test

---
+++ Jeune/Connexion.php
@@ -0,0 +1,201 @@
+<?php
+
+/*
+Une fonction qui permet de vérifier si un couple e-mail / mot de passe est correct.
+Elle prend les deux en paramètre.
+Renvoie 1 s'il est correct, 0 sinon.
+*/
+function veriflogin($email,$mdp){
+    // Ouverture du fichier de données et vérification.
+    $database=fopen("comptes.txt","r");
+    if(!$database){
+        return 0;
+    }
+    // Initialisation des variables de parcours.
+    $buffer="123";
+    $tab=[];
+    // Quand fgets est à la fin du fichier ou produit une erreur, elle renvoie false, ce qui sert ici pour la boucle.
+    while($buffer!=false){
+        $buffer=fgets($database);
+        if($buffer!=false){
+            // Séparation de chaque ligne en plusieurs morceaux grâce aux virgules.
+            $tab=explode(",",$buffer);
+            // Une condition en plus pour la sécurité.
+            // Il peut arriver que buffer soit vide s'il y a une ligne vide dans le fichier de données.
+            if(isset($tab[3])){
+                // $tab[3] est l'email de la ligne
+                if($email==$tab[3]){
+                    // Vérification du mot de passe
+                    // On ajoute le saut de ligne à la fin de $mdp car 
+                    // explode ne l'a pas enlevé automatiquement de $tab[4]
+                    if($tab[4]==$mdp."\n"){
+                        fclose($database);
+                        return 1;
+                    }
+                    else{
+                        fclose($database);
+                        return 0;
+                    }
+                }
+            }   
+        }
+    }
+    fclose($database);
+    return 0;
+}
+
+function recherchenom($email){
+	// Ouverture du fichier de données et vérification.
+    $database=fopen("comptes.txt","r");
+    if(!$database){
+        return 0;
+    }
+    // Initialisation des variables de parcours.
+    $buffer="123";
+    $tab=[];
+    // Quand fgets est à la fin du fichier ou produit une erreur, elle renvoie false, ce qui sert ici pour la boucle.
+    while($buffer!=false){
+        $buffer=fgets($database);
+        if($buffer!=false){
+            // Séparation de chaque ligne en plusieurs morceaux grâce aux virgules.
+            $tab=explode(",",$buffer);
+            // Une condition en plus pour la sécurité.
+            // Il peut arriver que buffer soit vide s'il y a une ligne vide dans le fichier de données.
+            if(isset($tab[3])){
+                // $tab[3] est l'email de la ligne
+                if($email==$tab[3]){
+                    return $tab[0];
+                }
+            }   
+        }
+    }
+    fclose($database);
+    return 0;
+}
+
+function rechercheprenom($email){
+	// Ouverture du fichier de données et vérification.
+    $database=fopen("comptes.txt","r");
+    if(!$database){
+        return 0;
+    }
+    // Initialisation des variables de parcours.
+    $buffer="123";
+    $tab=[];
+    // Quand fgets est à la fin du fichier ou produit une erreur, elle renvoie false, ce qui sert ici pour la boucle.
+    while($buffer!=false){
+        $buffer=fgets($database);
+        if($buffer!=false){
+            // Séparation de chaque ligne en plusieurs morceaux grâce aux virgules.
+            $tab=explode(",",$buffer);
+            // Une condition en plus pour la sécurité.
+            // Il peut arriver que buffer soit vide s'il y a une ligne vide dans le fichier de données.
+            if(isset($tab[3])){
+                // $tab[3] est l'email de la ligne
+                if($email==$tab[3]){
+                    return $tab[1];
+                }
+            }   
+        }
+    }
+    fclose($database);
+    return 0;
+}
+
+function rechercheddn($email){
+	// Ouverture du fichier de données et vérification.
+    $database=fopen("comptes.txt","r");
+    if(!$database){
+        return 0;
+    }
+    // Initialisation des variables de parcours.
+    $buffer="123";
+    $tab=[];
+    // Quand fgets est à la fin du fichier ou produit une erreur, elle renvoie false, ce qui sert ici pour la boucle.
+    while($buffer!=false){
+        $buffer=fgets($database);
+        if($buffer!=false){
+            // Séparation de chaque ligne en plusieurs morceaux grâce aux virgules.
+            $tab=explode(",",$buffer);
+            // Une condition en plus pour la sécurité.
+            // Il peut arriver que buffer soit vide s'il y a une ligne vide dans le fichier de données.
+            if(isset($tab[3])){
+                // $tab[3] est l'email de la ligne
+                if($email==$tab[3]){
+                    return $tab[2];
+                }
+            }   
+        }
+    }
+    fclose($database);
+    return 0;
+}
+
+$erreur=0;
+
+// Les variables du formulaire
+$email=$_POST['email'];
+$mdp=$_POST['mdp'];
+
+// Si l'utilisateur a réussi sa connexion, on initialise une session et on l'envoie sur son espace avec header
+if(veriflogin($email,$mdp)==1){
+    session_start();
+	$_SESSION['derniereconnexion']=time();
+    $_SESSION['email']=$email;
+    $_SESSION['nom']=recherchenom($email);
+    $_SESSION['prenom']=rechercheprenom($email);
+    $_SESSION['ddn']=rechercheddn($email);
+else{
+    $erreur=1;
+}
+?>
+
+
+<!DOCTYPE html>
+<!--HTML DE LA PAGE JEUNE DU MODULE VISITEUR-->
+<html>
+<head>
+    <title> Connexion - Jeunes 6.4</title>
+    <link rel="stylesheet" href="Connexion.css">
+    <!--Les attributs name et content permettent ici d'utiliser les unités dynamiques vw,vh,vmin... dans le CSS-->
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body class="nomargin">
+    <!--Une div qui correspond à l'entête des pages du site.-->
+    <div id="entete">
+        <a href="Presentation.html"> <img src="Images/logo1.png" id="imageentete"> </a>
+        <h1 id="titreentete1"> JEUNE </h1>
+        <p id="titreentete2"> Je donne de la valeur à mon engagement </p>
+    </div>
+
+    <!--La zone principale de la page.-->
+    <div id="corps">
+
+        <!--Le fond de la page est la version "jeune" du logo de JEUNES 6.4-->
+        <img id="fond" src="Images/logo2.JPG">
+
+        <!--La zone du formulaire de connexion-->
+        <div id="login">
+            <h2 id="titre"> Accéder à son espace Jeune </h2>
+            <form action="Connexion.php" method="post">
+                <label for="email">Adresse e-mail : </label>
+                <input type="email" id="email" name="email" required><br><br>
+                <label for="mdp">Mot de passe : </label>
+                <input type="password" id="mdp" name="mdp" required><br><br>
+                <input type="submit" id="boutonformulaire" value="Connexion">
+            </form>
+        </div>
+
+        <!--La zone de l'annonceur, utilisée pour transmettre des messages via PHP-->
+        <div id="annonceur"> 
+            <?php
+                // Si l'utilisateur n'est pas redirigé par le code PHP d'avant, c'est forcément qu'il y a eu une erreur
+                echo "<p>L'adresse e-mail ou mot de passe est incorrect.</p>";
+            ?>
+        </div>
+    </div>
+
+
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,10 +1,60 @@
 # **Projet-Jeunes-6.4**
 
-#### **--DESCRIPTION--**
-Le projet Jeunes 6.4 est un dispositif de valorisation de l'engagement des jeunes en Pyrénées-Atlantiques soutenu par l'État, le Conseil Général, le Conseil Régional, les CAF Béarn Soule et Pays Basque, la MSA, la CPAM.
+#### **--DESCRIPTION--**  
+
+Le projet Jeunes 6.4 est un dispositif de valorisation de l'engagement des jeunes en Pyrénées-Atlantiques soutenu par l'État, le Conseil Général, le Conseil Régional, les CAF Béarn Soule et Pays Basque, la MSA, la CPAM.  
+
+Jeunes 6.4 est un site web créé avec HTML, CSS, Javascript, PHP.  
 
 #### **--COMMENT L'UTILISER--**
 
+1. Téléchargez tous les fichiers du projet et mettez les dans un dossier de votre choix.  
+2. Démarrez un serveur local dans le dossier concerné, avec xampp par exemple.  
+3. Vous pouvez commencer la navigation sur le site à l'adresse localhost/Portail.html  
+
+Les modules Référent et Consultant ne sont accessibles que par le suivi d'un lien envoyé par e-mail.
+
 #### **--LISTE DES FICHIERS--**
+
++ MODULE VISITEUR
+  - Portail.html  
+  - Portail.css  
+  - Presentation.html  
+  - Presentation.css  
+  - Jeune.html  
+  - Jeune.css  
+  - Referent.html  
+  - Referent.css  
+  - Consultant.html  
+  - Consultant.css  
+  - Partenaires.html  
+  - Partenaires.css  
+  - Inscription.html  
+  - Inscription.php  
+  - Inscription.css  
+  - Connexion.html  
+  - Connexion.css  
+  - Connexion.php  
++ MODULE JEUNE
+  - CompteJeune.php
+  - CompteJeune.css
+  - ModificationProfil.php
+  - ModificationProfil.css
+  - DemandeDeReference.php
+  - DemandeDeReference.css
+  - ListeReferences.php
+  - ListeReferences.css
++ MODULE RÉFÉRENT 
+  - ResumeDemandeDeReference.php  
+  - ResumeDemandeDeReference.css 
+  - ModificationInfosReferent.php
+  - ModificationInfosReferent.css
+  - PageDeConfirmation.html  
+  - PageDeConfirmation.css  
++ MODULE CONSULTANT  
+  - ResumeConsultant.php
+  - ResumeConsultant.css
+
+Plus de détails sur l'architecture du projet sont à trouver dans le rapport joint.
 
 > **Aurélie Feraud & Alexandre Grisez \ PréIng2 G6-MI \ CY TECH**
